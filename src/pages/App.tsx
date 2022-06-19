fefbd8bb8b79b5507e99040e953883d3333d9662
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-06-19 17:29:45 -0300

    feat: add images and json

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from 'react'
 import {Header} from '../styles/components'
+import json from '../db/data.json'
+
+type Job = {
+  id: number,
+  company: string,
+  logo: string,
+  new: boolean,
+  featured: boolean,
+  position: string,
+  role: string,
+  level: string,
+  postedAt: string,
+  contract: string,
+  location: string,
+  languages: Array<string>,
+  tools: Array<string>
+}
 
 function App() {
+  const [jobs,setJobs] = useState<null|Array<Job>>(null)
+
+  useEffect(()=>{
+    setJobs(json)
+  })
+
   return (
     <div >
       <Header />
-      JobList
+      {
+        jobs&&jobs.map(job=>(
+          <div key={job.id}>
+            <h1>{job.position}</h1>
+            <img src={job.logo} alt="logo" />
+            <p>{job.company}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
